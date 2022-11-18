8cf8233eb13e72d87c5c8b7dfb58c58feab67cc3
Jessica Vaughn <jessica.vaughn619@gmail.com>
2022-11-18 15:12:40 -0700

    solution

--- index.js
+++ index.js
@@ -3,18 +3,25 @@ const employee = {
     streetAddress: "123 Mainstreet"
 };
 
-function updateEmployeeWithKeyAndValue() {
-
+function updateEmployeeWithKeyAndValue(employee, streetAddress) {
+    const newEmployee = {...employee};
+    newEmployee.streetAddress = "11 Broadway";
+    return newEmployee;
 };
 
 function destructivelyUpdateEmployeeWithKeyAndValue() {
-
+    employee.name = "Sam"; 
+    employee.streetAddress = "12 Broadway"
+    return employee;
 };
 
 function deleteFromEmployeeByKey() {
-
+    const newEmployee = {...employee};
+    delete newEmployee.name;
+    return newEmployee;
 };
 
 function destructivelyDeleteFromEmployeeByKey() {
-
+    delete employee.name;
+    return employee;
 };
